4e9427f3db8baf780fa4fddf72bebac248f1900b
Lesuuh <lesuuh01@gmail.com>
2025-03-31 22:55:33 +0100

    Add PriceSlider component and related styles; introduce RadioGroup and Slider components for enhanced UI functionality

---
+++ src/components/PriceSlider.tsx
@@ -0,0 +1,15 @@
+import "./price.css";
+
+const PriceSlider = () => {
+  return (
+    <div>
+      <div className="double-slider-box">
+        <h3 className="range-title">Price Range Slider</h3>
+        <div className="range-slider"></div>
+        <div className="input-box"></div>
+      </div>
+    </div>
+  );
+};
+
+export default PriceSlider;
--- src/pages/ShopPage.tsx
+++ src/pages/ShopPage.tsx
@@ -1,7 +1,19 @@
+import { HomeIcon } from "@/components/icons/HomeIcon";
+import PriceSlider from "@/components/PriceSlider";
 import ProductCard from "@/components/products/ProductCard";
-import { ProductTypes } from "@/lib/types";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { CategoriesTypes, ProductTypes } from "@/lib/types";
+import { Separator } from "@radix-ui/react-select";
 
 import { useQuery } from "@tanstack/react-query";
+import { ChevronRight } from "lucide-react";
+import { Link } from "react-router";
 
 const fetchProducts = async () => {
   const response = await fetch("http://localhost:8000/products");
@@ -9,29 +21,116 @@ const fetchProducts = async () => {
   return data;
 };
 
+const fetchCategories = async () => {
+  const response = await fetch("http://localhost:8000/categories");
+  const data = response.json();
+  return data;
+};
+
 const ShopPage = () => {
-  const { data, isLoading, error } = useQuery({
+  const {
+    data: products,
+    isLoading: productsLoading,
+    error: productsError,
+  } = useQuery({
     queryKey: ["products"],
     queryFn: fetchProducts,
   });
 
-  if (error) {
-    return <p>{error.message}</p>;
+  const {
+    data: categories,
+    isLoading: categoriesLoading,
+    error: categoriesError,
+  } = useQuery({
+    queryKey: ["categories"],
+    queryFn: fetchCategories,
+  });
+
+  if (productsError || categoriesError) {
+    return <p>{productsError?.message || categoriesError?.message}</p>;
   }
 
-  if (isLoading) {
-    return <p>Loading Products</p>;
+  if (productsLoading || categoriesLoading) {
+    return <p>Loading {productsLoading ? "products" : "categories"}</p>;
   }
 
   return (
-    <div>
-      <h1>Shop page</h1>
-      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4  border border-gray-200 w-full">
-        {data?.map((item: ProductTypes) => (
-          <ProductCard item={item} />
-        ))}
+    <section className="my-10 ">
+      <div className="bg-gray-50 max-w-[1400px]  py-3 mb-5 gap-4 px-4 md:px-20 w-full mx-auto">
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <Link to="/">
+                <BreadcrumbLink className="flex items-end text-gray text-[.7rem]">
+                  <HomeIcon className="text-gray w-3 mr-1" />
+                  Home
+                </BreadcrumbLink>
+              </Link>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator>
+              <ChevronRight absoluteStrokeWidth className="text-gray w-2" />
+            </BreadcrumbSeparator>
+            <BreadcrumbItem>
+              <Link to="/shop">
+                <BreadcrumbLink className="text-gray  text-[.7rem]">
+                  Shop
+                </BreadcrumbLink>
+              </Link>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
       </div>
-    </div>
+      <div className="flex px-4 md:px-20 w-full mx-auto max-w-[1400px] gap-4">
+        <aside className="w-[30%]">
+          <div>
+            <h2 className="text-xs font-medium">CATEGORY</h2>
+            <ul className="mt-2">
+              {categories.map((cat: CategoriesTypes) => (
+                <label
+                  key={cat.id}
+                  htmlFor={`radioInput${cat.id}`}
+                  className="flex items-center text-gray-500 font-normal pb-2"
+                >
+                  <input
+                    type="radio"
+                    name="category"
+                    id={`radioInput${cat.id}`}
+                    className="sr-only peer"
+                  />
+                  <div className="w-3 h-3 bg-white border border-gray-300 rounded-full  peer-checked:border-primary peer-checked:border-[3.5px]"></div>
+                  <li className="text-[.7rem] ml-2 peer-checked:text-black peer-checked:font-bold-">
+                    {cat.name}
+                  </li>
+                </label>
+              ))}
+            </ul>
+          </div>
+          <Separator className="w-full border-t border-gray-300" />
+          <div>
+            <h2 className="text-xs font-medium">PRICE TAG</h2>
+          </div>
+          <Separator className="w-full border-t border-gray-300" />
+          <div>
+            <h2 className="text-xs font-medium">POPULAR BRANDS</h2>
+          </div>
+          <Separator className="w-full border-t border-gray-300" />
+          <div>
+            <h2 className="text-xs font-medium">POPULAR TAGS</h2>
+          </div>
+        </aside>
+
+        {/* main */}
+        <main className="">
+          <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4  gap-4 w-full">
+            {products?.map((item: ProductTypes) => (
+              <ProductCard item={item} />
+            ))}
+          </div>
+        </main>
+      </div>
+      <div>Shop page</div>
+      <PriceSlider />
+    </section>
   );
 };
 
