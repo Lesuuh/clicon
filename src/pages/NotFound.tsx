51e7788440a2bcb251179c8b8d064009bbabe614
Lesuuh <lesuuh01@gmail.com>
2025-04-04 12:05:03 +0100

    Enhance NotFound page with improved layout, messaging, and navigation buttons

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,12 +1,38 @@
+import { HomeIcon } from "@/components/icons/HomeIcon";
+import { Button } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
+import { Link } from "react-router";
+
 const NotFound = ({ message }: { message: string }) => {
   return (
-    <div className="flex justify-center items-center w-full">
-      <h2 className="text-center">{message}</h2>
+    <section className=" my-10 mx-auto w-full max-w-[1400px] px-5 md:px-20 flex justify-center flex-col items-center  ">
       <img
         src="/images/Oops! 404 Error with a broken robot-rafiki (1) 1.png"
-        alt=""
+        alt="404 image"
+        className="w-66"
       />
-    </div>
+      <h2 className="text-2xl my-2">404, {message}</h2>
+      <p className="text-gray text-[.8rem] text-center my-2">
+        Something went wrong. It’s look that your requested could not be found.
+        It’s look like the link is broken or the page is removed.
+      </p>
+      <div className="flex justify-center items-center flex-col md:flex-row gap-2 mt-2 w-full">
+        <Button
+          onClick={() => window.history.back()}
+          className="w-32 cursor-pointer text-white text-[.7rem] rounded-xs"
+        >
+          <ArrowLeft /> GO BACK
+        </Button>
+        <Link to="/">
+          <Button
+            variant={"outline"}
+            className="w-32 cursor-pointer text-[.7rem] rounded-xs border-primary border text-primary"
+          >
+            <HomeIcon className="text-primary outline-primary" /> GO TO HOME
+          </Button>
+        </Link>
+      </div>
+    </section>
   );
 };
 
