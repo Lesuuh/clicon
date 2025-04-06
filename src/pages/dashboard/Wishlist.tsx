4ae9f6525b9585e8a29cd9b269f2c866a7d04fe3
Lesuuh <lesuuh01@gmail.com>
2025-04-06 19:48:22 +0100

    Refactor Wishlist component to remove hardcoded wishlist data; update product rendering logic to display fetched products

--- src/pages/dashboard/Wishlist.tsx
+++ src/pages/dashboard/Wishlist.tsx
@@ -2,12 +2,13 @@ import ClipLoaderSpinner from "@/components/icons/ClipLoaderSpinner";
 import ProductCard from "@/components/products/ProductCard";
 import { ProductTypes } from "@/lib/types";
 import { useQuery } from "@tanstack/react-query";
+// import { useEffect, useState } from "react";
 import { Link } from "react-router";
 
-const wishlist = [
-  { id: 1, productId: 2 },
-  { id: 2, productId: 1 },
-];
+// const wishlist = [
+//   { id: 1, productId: 2 },
+//   { id: 2, productId: 1 },
+// ];
 const fetchProducts = async () => {
   const response = await fetch("http://localhost:8000/products");
   const data = await response.json();
@@ -19,6 +20,20 @@ const Wishlist = () => {
     queryKey: ["products"],
     queryFn: fetchProducts,
   });
+
+  // const wishlistProducts: ProductTypes[] = [];
+  // // const wishlistIds = wishlist.map((id) => id.productId);
+
+  // wishlist.forEach((wishlistItem) => {
+  //   const matchingProduct = data?.find(
+  //     (product: ProductTypes) => product.id === wishlistItem.productId
+  //   );
+  //   if (matchingProduct) {
+  //     wishlistProducts.push(matchingProduct);
+  //   }
+  // });
+
+  // console.log(wishlistProducts);
 
   if (isLoading) {
     return (
@@ -36,27 +51,19 @@ const Wishlist = () => {
     );
   }
 
-  const wishlistIds = wishlist.map((item) => item.productId);
-
-  const wishlistProducts = data.filter((product: ProductTypes) =>
-    wishlistIds.includes(product.id)
-  );
-
-  console.log(data);
-
-  console.log(wishlistProducts);
   return (
     <div className="w-full">
       <h2 className="w-full bg-gray-200 py-1 rounded-xs px-3 text-[.8rem]">
-        WISHLIST (8)
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-          {wishlistProducts.map((item: ProductTypes) => (
+        WISHLIST (8){" "}
+      </h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+        {data &&
+          data.map((item: ProductTypes) => (
             <Link key={item.id} to={`products/${item.id}`}>
               <ProductCard item={item} />
             </Link>
           ))}
-        </div>
-      </h2>
+      </div>
     </div>
   );
 };
