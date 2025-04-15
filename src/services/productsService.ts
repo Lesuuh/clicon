0059a94d6e84fa496c507f42103eb1d29dbff625
Ueh-Kabari Lesuuh <103704233+Lesuuh@users.noreply.github.com>
2025-04-15 14:27:11 +0100

    Update productsService.ts

--- src/services/productsService.ts
+++ src/services/productsService.ts
@@ -1,5 +1,8 @@
-export const fetchProducts =()=> {
- const snapshot = getdocs(collections(db, "products"))
+import {db} from "firebase"
+import {collections, getDocs} from "firebase/firestore"
+
+export const fetchProducts = async()=> {
+ const snapshot = await getdocs(collections(db, "products"))
 
 const products = snapshot.docs.map((doc)=> id:doc.id, doc.data())
 return products
