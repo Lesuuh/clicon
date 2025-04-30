c32bbf5e7d46443ba7196d1d164cb226d24d3328
Lesuuh <lesuuh01@gmail.com>
2025-04-30 08:19:41 +0100

    fix the ui of the wishlist page

--- src/pages/dashboard/Wishlist.tsx
+++ src/pages/dashboard/Wishlist.tsx
@@ -2,11 +2,13 @@ import ProductCard from "@/components/products/ProductCard";
 import { Button } from "@/components/ui/button";
 import { ProductTypes } from "@/lib/types";
 import { useWishlistStore } from "@/store/wishlistStore";
+import { useState } from "react";
 import { Link, useNavigate } from "react-router";
 
 const Wishlist = () => {
   const wishlist = useWishlistStore((state) => state.wishlist);
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
 
   // if (error) {
   //   return (
@@ -15,21 +17,30 @@ const Wishlist = () => {
   //     </div>
   //   );
   // }
+  if (loading) {
+    return (
+      <p className="flex justify-center items-center flex-row w-full">
+        Loading...
+      </p>
+    );
+  }
 
   return (
     <div className="w-full">
       <h2 className="w-full bg-gray-200 py-1 rounded-xs px-3 text-[.8rem]">
         WISHLIST ({wishlist.length})
       </h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-        {wishlist.length !== 0 ? (
-          wishlist.map((item: ProductTypes) => (
+      {wishlist.length !== 0 ? (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 w-full">
+          {wishlist.map((item: ProductTypes) => (
             <Link key={item.id} to={`/products/${item.id}`}>
               <ProductCard item={item} />
             </Link>
-          ))
-        ) : (
-          <div className="flex w-full flex-col h-full items-center text-[.8rem] font-semibold justify-center">
+          ))}
+        </div>
+      ) : (
+        <div className="flex w-full flex-col h-full items-center text-[.8rem] font-semibold justify-center">
+          <div className="flex w-full items-center justify-center flex-col">
             <p>No Products in Wishlist, add your favourites</p>
             <Button
               onClick={() => navigate("/products")}
@@ -38,8 +49,8 @@ const Wishlist = () => {
               Go Shopping
             </Button>
           </div>
-        )}
-      </div>
+        </div>
+      )}
     </div>
   );
 };
